1f4ba49b3068989ac37a01547f5f079d6e82312c
“uday <bishtuday4668@gmail.com>
2025-06-12 10:50:50 +0530

    creating the index file for db connection

--- models/index.js
+++ models/index.js
@@ -2,18 +2,33 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require('../config/config.js')[env];
 
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+try {
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], {
+      ...config,
+      dialect: 'postgres',
+    });
+  } else {
+    sequelize = new Sequelize(config.database, config.username, config.password, {
+      ...config,
+      dialect: 'postgres',
+    });
+  }
+
+  sequelize.authenticate()
+    .then(() => console.log('Database connected successfully.'))
+    .catch(err => console.error('Database connection error:', err));
+} catch (error) {
+  console.error('Sequelize initialization error:', error);
+  throw error;
 }
 
 fs
@@ -24,13 +39,25 @@ fs
     file.slice(-3) === '.js'
   )
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+    try {
+      const model = require(path.join(__dirname, file))(sequelize, DataTypes);
+      if (!model || !model.name) {
+        console.error(`Model in ${file} is invalid or missing name.`);
+        return;
+      }
+      db[model.name] = model;
+    } catch (error) {
+      console.error(`Error loading model ${file}:`, error);
+    }
   });
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
-    db[modelName].associate(db);
+    try {
+      db[modelName].associate(db);
+    } catch (error) {
+      console.error(`Error in associate for ${modelName}:`, error);
+    }
   }
 });
 
